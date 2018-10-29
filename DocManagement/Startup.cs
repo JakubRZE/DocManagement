ff18db2e6c10863787384474363c820723910f97
Jakub Rzepka <Jakubjakk@gmail.com>
2018-10-29 15:17:05 +0100

    Add project files.

---
+++ DocManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DocManagement.Startup))]
+namespace DocManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
