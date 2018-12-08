dd48c47f8bd6efd30097e8957fff9d5a0c57e5f0
Jakub Rzepka <Jakubjakk@gmail.com>
2018-12-08 18:30:08 +0100

    Revert "identityModel update"
    This reverts commit ab63eeff7dfbef0d528326572e2f7c3c61411081.

--- DocManagement/Models/AccountViewModels.cs
+++ DocManagement/Models/AccountViewModels.cs
@@ -64,28 +64,6 @@ namespace DocManagement.Models
 
     public class RegisterViewModel
     {
-
-        [Required]
-        [Display(Name = "First Name")]
-        public string FirstName { get; set; }
-
-        [Required]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-
-        [Required]
-        [Display(Name = "Address")]
-        public string Address { get; set; }
-
-        [Required]
-        [Display(Name = "Position")]
-        public string Position { get; set; }
-
-        [Required]
-        [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; }
-
-
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
--- DocManagement/Models/IdentityModels.cs
+++ DocManagement/Models/IdentityModels.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -12,30 +9,10 @@ namespace DocManagement.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Login { get; set; }
-
-        [Required]
-        public string FirstName { get; set; }
-        [Required]
-        public string LastName { get; set; }
-        [Required]
-        public string Address { get; set; }
-        [Required]
-        public string Position { get; set; }
-
-        [Required]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        // not supose to be here \/
-        [DisplayFormat(DataFormatString = "{0:dd MMMM yyyy}")]
-        public DateTime RegistrationDate { get; set; }
-
-
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
-        // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-        var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
         }
