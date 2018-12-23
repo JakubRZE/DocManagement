a50686bd29fd8f5a99f84d9408e2eb712436f34d
Jakub Rzepka <Jakubjakk@gmail.com>
2018-12-23 23:11:49 +0100

    Views update

--- DocManagement/Controllers/HomeController.cs
+++ DocManagement/Controllers/HomeController.cs
@@ -14,18 +14,18 @@ namespace DocManagement.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+        //    return View();
+        //}
     }
 }
