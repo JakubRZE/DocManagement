80f67bca28b29841ef84417c33b17672eeb4ae7d
Jakub Rzepka <Jakubjakk@gmail.com>
2018-12-28 00:19:49 +0100

    Activity update

--- DocManagement/Controllers/ActivityController.cs
+++ DocManagement/Controllers/ActivityController.cs
@@ -7,6 +7,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DocManagement.Models;
+using DocManagement.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace DocManagement.Controllers
 {
@@ -15,11 +17,29 @@ namespace DocManagement.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Activity
+        [Authorize]
         public ActionResult Index()
         {
-            var documents = db.Documents.Include(d => d.ApplicationUser).Include(d => d.Download);
-            return View(documents.ToList());
+
+            var userId = User.Identity.GetUserId();
+
+            var upload = db.Documents.Where(u => u.ApplicationUserId == userId).ToList();
+            var download =  db.Downloads.Include(x => x.Document).Where(u => u.ApplicationUserId == userId).Select(x => x.Document).ToList();
+
+            var ActivityVM= new ActivityViewModel();
+            ActivityVM.Uploads = upload;
+            ActivityVM.Downloads = download;
+
+            return View(ActivityVM);
         }
+
+
+
+
+
+
+
+
 
         //// GET: Activity/Details/5
         //public ActionResult Details(int? id)
---
+++ DocManagement/ViewModels/ActivityViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DocManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocManagement.ViewModels
+{
+    public class ActivityViewModel
+    {
+        public List<Document> Uploads { get; set; }
+        public List<Document> Downloads { get; set; }
+    }
+}
