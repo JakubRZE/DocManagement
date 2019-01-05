8f9c5a398c5cdccbbb386e4cdce7b08d8d724fd5
Jakub Rzepka <Jakubjakk@gmail.com>
2019-01-05 23:29:57 +0100

    Home update

--- DocManagement/Controllers/HomeController.cs
+++ DocManagement/Controllers/HomeController.cs
@@ -1,26 +1,33 @@
-﻿using System;
+﻿using DocManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DocManagement.ViewModels;
 
 namespace DocManagement.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Home
+        [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
         // GET:  DocumentsReport
+        [Authorize]
         public ActionResult DocumentsReport()
         {
             return View();
         }
 
         // GET: EmployeesReport
+        [Authorize]
         public ActionResult EmployeesReport()
         {
             return View();
